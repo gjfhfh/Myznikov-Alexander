fa14cf279403d6e8e282642bc34292388151053a
Myznikov-Alexander <a.myznikov2006@yandex.ru>
2024-12-14 00:29:12 +0300

    52

--- src/main/java/org/example/Article/ArticleController.java
+++ src/main/java/org/example/Article/ArticleController.java
@@ -1,25 +1,48 @@
 package org.example.Article;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.ConnectionDataBaseException;
+import org.example.Article.Request.ArticleCreateRequest;
+import org.example.Article.Request.ArticleUpdateRequest;
+import org.example.Article.Response.ArticleUpdateResponse;
+import org.example.Article.Response.ArticlesCreateResponse;
+import org.example.Article.Response.ArticleAllFindResponse;
+import org.example.Article.Response.ArticleCreateResponse;
+import org.example.Article.Response.ArticleDeleteResponse;
+import org.example.Article.Response.ArticleFindResponse;
 import org.example.Comment.CommentService;
+import org.example.Article.Exceptions.ArticleCreateException;
+import org.example.Article.Exceptions.ArticleFindException;
+import org.example.Controller;
+import org.example.ErrorResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import spark.Request;
 import spark.Response;
 import spark.Service;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 
 public class ArticleController implements Controller {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ArticleController.class);
 
     private final Service service;
     private final ArticleService articleService;
     private final CommentService commentService;
     private final ObjectMapper objectMapper;
-    private final ArticleRepository articleRepository; // Объявление переменной
 
-    public ArticleController(Service service, ArticleService articleService, CommentService commentService, ObjectMapper objectMapper, ArticleRepository articleRepository) {
+    public ArticleController(Service service, ArticleService articleService, CommentService commentService, ObjectMapper objectMapper) {
         this.service = service;
         this.articleService = articleService;
         this.commentService = commentService;
         this.objectMapper = objectMapper;
-        this.articleRepository = articleRepository; // Инициализация
     }
 
     @Override
@@ -29,44 +52,41 @@ public class ArticleController implements Controller {
         getAllArticles();
         deleteArticle();
         updateArticle();
-    }
-
-    private void createMultipleArticles() {
-        service.post(
-                "/api/articles/multiple",
-                (Request request, Response response) -> {
-                    response.type("application/json");
-                    List<ArticleCreateRequest> articleRequests = objectMapper.readValue(request.body(),
-                            new TypeReference<List<ArticleCreateRequest>>() {
-                            });
-                    List<Article> articles = new ArrayList<>();
-                    for (ArticleCreateRequest req : articleRequests) {
-                        articles.add(new Article(articleRepository.generateId(), req.title(), req.tags(), new ArrayList<>()));
-                    }
-                    articleRepository.createMultiple(articles);
-                    response.status(201);
-                    return objectMapper.writeValueAsString(articles);
-                }
-        );
+        createArticles();
     }
 
     private void createArticle() {
         service.post(
-                "/api/articles",
+                "/api/article",
                 (Request request, Response response) -> {
                     response.type("application/json");
+
                     String body = request.body();
-                    ArticleCreateRequest articleCreateRequest = objectMapper.readValue(body,
-                            ArticleCreateRequest.class);
+
+                    ArticleCreateRequest articleCreateRequest;
+
                     try {
-                        ArticleId articleId = articleService.create(articleCreateRequest.title(),
-                                articleCreateRequest.tags());
-                        LOG.debug("Article created: {}", articleId);
+                        articleCreateRequest = objectMapper.readValue(body, ArticleCreateRequest.class);
+                    } catch (JsonProcessingException e) {
+                        LOG.warn(e.getMessage());
+                        response.status(400);
+                        return objectMapper.writeValueAsString(new ErrorResponse(e.getMessage()));
+                    }
+
+                    try {
+                        Optional<Long> articleId = articleService.generateId();
+                        Article article = new Article(articleId.get(), articleCreateRequest.title(), articleCreateRequest.tags(), null, false);
+                        articleService.create(article);
+                        LOG.debug("Article with id = {} created", articleId);
                         response.status(200);
-                        return objectMapper.writeValueAsString(new ArticleCreateResponse(articleId));
+                        return objectMapper.writeValueAsString(new ArticleCreateResponse(articleId.get()));
                     } catch (ArticleCreateException e) {
                         LOG.warn("Cannot create article", e);
                         response.status(400);
+                        return objectMapper.writeValueAsString(new ErrorResponse(e.getMessage()));
+                    } catch (ConnectionDataBaseException e) {
+                        LOG.error(e.getMessage());
+                        response.status(500);
                         return objectMapper.writeValueAsString(new ErrorResponse(e.getMessage()));
                     }
                 }
@@ -75,10 +95,19 @@ public class ArticleController implements Controller {
 
     private void getArticle() {
         service.get(
-                "/api/articles/:articleId",
+                "/api/article/:articleId",
                 (Request request, Response response) -> {
                     response.type("application/json");
-                    ArticleId articleId = new ArticleId(Long.parseLong(request.params("articleId")));
+                    Long articleId;
+
+                    try {
+                        articleId = Long.parseLong(request.params("articleId"));
+                    } catch (NumberFormatException e) {
+                        LOG.warn(e.getMessage());
+                        response.status(400);
+                        return objectMapper.writeValueAsString(new ErrorResponse(e.getMessage()));
+                    }
+
                     Article article;
 
                     try {
@@ -89,7 +118,7 @@ public class ArticleController implements Controller {
                         return objectMapper.writeValueAsString(new ErrorResponse(e.getMessage()));
                     }
 
-                    LOG.debug("Article found: {}", articleId);
+                    LOG.debug("Article with id = {} found", articleId);
                     response.status(200);
                     return objectMapper.writeValueAsString(new ArticleFindResponse(
                             articleId.toString(),
@@ -103,37 +132,61 @@ public class ArticleController implements Controller {
 
     private void getAllArticles() {
         service.get(
-                "/api/all-articles",
+                "/api/articles",
                 (Request request, Response response) -> {
                     response.type("application/json");
-                    List<Article> articles = articleService.findAll();
-                    Map<String, ArticleAllFindResponse> articlesMap = new HashMap<>();
 
-                    for (Article article : articles) {
-                        articlesMap.put(article.getId().toString(), new ArticleAllFindResponse(
-                                article.getTitle(),
-                                article.getTags(),
-                                article.getComments()
-                        ));
+                    Optional<List<Article>> articles;
+
+                    try {
+                        articles = articleService.findAll();
+                    } catch (ConnectionDataBaseException e) {
+                        LOG.error(e.getMessage());
+                        response.status(500);
+                        return objectMapper.writeValueAsString(new ErrorResponse(e.getMessage()));
                     }
 
-                    LOG.debug("Successfully retrieved all articles");
-                    response.status(200);
-                    return objectMapper.writeValueAsString(articlesMap);
+                    Map<String, ArticleAllFindResponse> articlesMap = new HashMap<>();
+
+                    if (articles.isPresent()) {
+                        for (Article article : articles.get()) {
+                            articlesMap.put(article.getId().toString(), new ArticleAllFindResponse(
+                                    article.getTitle(),
+                                    article.getTags(),
+                                    article.getComments()
+                            ));
+                        }
+
+                        LOG.debug("Successfully retrieved all articles");
+                        response.status(200);
+                        return objectMapper.writeValueAsString(articlesMap);
+                    } else {
+                        LOG.debug("Successfully retrieved all articles");
+                        response.status(200);
+                        return objectMapper.writeValueAsString(articlesMap);
+                    }
                 }
         );
     }
 
     private void deleteArticle() {
         service.delete(
-                "/api/delete-article/:articleId",
+                "/api/article/:articleId",
                 (Request request, Response response) -> {
                     response.type("application/json");
-                    ArticleId articleId = new ArticleId(Long.parseLong(request.params("articleId")));
-                    Article article;
+
+                    Long articleId;
 
                     try {
-                        article = articleService.findById(articleId);
+                        articleId = Long.parseLong(request.params("articleId"));
+                    } catch (NumberFormatException e) {
+                        LOG.warn(e.getMessage());
+                        response.status(400);
+                        return objectMapper.writeValueAsString(new ErrorResponse(e.getMessage()));
+                    }
+
+                    try {
+                        articleService.findById(articleId);
                     } catch (ArticleFindException e) {
                         LOG.warn("Article not found", e);
                         response.status(404);
@@ -150,13 +203,30 @@ public class ArticleController implements Controller {
 
     private void updateArticle() {
         service.put(
-                "api/update-article/:articleId",
+                "api/article/:articleId",
                 (Request request, Response response) -> {
                     response.type("application/json");
                     String body = request.body();
-                    ArticleUpdateRequest articleUpdateRequest = objectMapper.readValue(body, ArticleUpdateRequest.class);
 
-                    ArticleId articleId = new ArticleId(Long.parseLong(request.params("articleId")));
+                    ArticleUpdateRequest articleUpdateRequest;
+                    Long articleId;
+
+                    try {
+                        articleUpdateRequest = objectMapper.readValue(body, ArticleUpdateRequest.class);
+                    } catch (JsonProcessingException e) {
+                        LOG.warn(e.getMessage());
+                        response.status(400);
+                        return objectMapper.writeValueAsString(new ErrorResponse(e.getMessage()));
+                    }
+
+                    try {
+                        articleId = Long.parseLong(request.params("articleId"));
+                    } catch (NumberFormatException e) {
+                        LOG.warn(e.getMessage());
+                        response.status(400);
+                        return objectMapper.writeValueAsString(new ErrorResponse(e.getMessage()));
+                    }
+
                     Article article;
 
                     try {
@@ -167,10 +237,63 @@ public class ArticleController implements Controller {
                         return objectMapper.writeValueAsString(new ErrorResponse(e.getMessage()));
                     }
 
-                    articleService.update(articleId, articleUpdateRequest.title(), articleUpdateRequest.tags());
+                    article = article.withTags(articleUpdateRequest.tags());
+                    article = article.withTitle(articleUpdateRequest.title());
+
+                    articleService.update(article);
                     LOG.debug("Article updated: {}", articleId);
                     response.status(200);
                     return objectMapper.writeValueAsString(new ArticleUpdateResponse(articleId));
+                }
+        );
+    }
+
+    public void createArticles() {
+        service.post(
+                "api/articles",
+                (Request request, Response response) -> {
+                    response.type("application/json");
+                    String body = request.body();
+
+                    List<ArticleCreateRequest> articlesRequest;
+
+                    try {
+                        articlesRequest = Arrays.asList(objectMapper.readValue(body, ArticleCreateRequest[].class));
+                    } catch (JsonProcessingException e) {
+                        LOG.warn(e.getMessage());
+                        response.status(400);
+                        return objectMapper.writeValueAsString(new ErrorResponse(e.getMessage()));
+                    }
+
+                    List<Article> articles = new ArrayList<>();
+
+                    try {
+                        for (ArticleCreateRequest article : articlesRequest) {
+                            articles.add(new Article(articleService.generateId().get(),
+                                    article.title(),
+                                    article.tags(),
+                                    null,
+                                    false
+                            ));
+                        }
+                    } catch (ConnectionDataBaseException e) {
+                        LOG.error(e.getMessage());
+                        response.status(500);
+                        return objectMapper.writeValueAsString(new ErrorResponse(e.getMessage()));
+                    }
+
+                    try {
+                        articleService.createArticles(articles);
+                    } catch (ConnectionDataBaseException e) {
+                        LOG.error(e.getMessage());
+                        response.status(500);
+                        return objectMapper.writeValueAsString(new ErrorResponse(e.getMessage()));
+                    }
+
+
+                    LOG.debug("Successfully created articles");
+                    response.status(200);
+                    return objectMapper.writeValueAsString(new ArticlesCreateResponse(articles.stream().map(x -> x.getId()).toList()));
                 }
         );
     }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,48 +1,64 @@
 package org.example;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.example.Article.ArticleController;
-import org.example.Article.ArticleService;
-import org.example.Article.InMemoryArticleRepository;
+import io.github.resilience4j.retry.Retry;
+import io.github.resilience4j.retry.RetryConfig;
+import org.example.Article.*;
 import org.example.Comment.CommentController;
+import org.example.Comment.CommentRepositoryImpl;
 import org.example.Comment.CommentService;
-import org.example.Comment.InMemoryCommentRepository;
+import org.example.HTML.ArticleFreemarkerController;
+import org.example.HTML.TemplateFactory;
+import org.flywaydb.core.Flyway;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.core.statement.UnableToExecuteStatementException;
 import spark.Service;
-
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
+        Config config = ConfigFactory.load();
+
+        Flyway flyway =
+                Flyway.configure()
+                        .locations("classpath:db/migrations")
+                        .dataSource(config.getString("app.database.url"), config.getString("app.database.user"),
+                                config.getString("app.database.password"))
+                        .load();
+        flyway.migrate();
+
+        Jdbi jdbi = Jdbi.create(config.getString("app.database.url"), config.getString("app.database.user"),
+                config.getString("app.database.password"));
+
         Service service = Service.ignite();
         ObjectMapper objectMapper = new ObjectMapper();
 
-        // Инициализация репозиториев
-        InMemoryArticleRepository inMemoryArticleRepository = new InMemoryArticleRepository();
-        InMemoryCommentRepository inMemoryCommentRepository = new InMemoryCommentRepository();
-
-        // Инициализация сервисов
-        ArticleService articleService = new ArticleService(inMemoryArticleRepository, inMemoryCommentRepository);
-        CommentService commentService = new CommentService(inMemoryCommentRepository, inMemoryArticleRepository);
-
-        // Инициализация контроллеров
-        ArticleController articleController = new ArticleController(
-                service,
-                articleService,
-                commentService,
-                objectMapper
+        ArticleService articleService = new RetryableArticleServiceImpl(
+                new ArticleServiceImpl(
+                        new ArticleRepositoryImpl(jdbi),
+                        new CommentRepositoryImpl(jdbi),
+                        new JdbiTransactionManager(jdbi)
+                ),
+                Retry.of(
+                        "retry-db",
+                        RetryConfig.custom()
+                                .maxAttempts(3)
+                                .retryExceptions(UnableToExecuteStatementException.class)
+                                .build()
+                )
         );
 
-        CommentController commentController = new CommentController(
-                service,
-                articleService,
-                commentService,
-                objectMapper
-        );
+        CommentService commentService = new CommentService(new CommentRepositoryImpl(jdbi));
 
-        // Запуск приложения
         Application application = new Application(
-                List.of(articleController, commentController)
+                List.of(
+                        new ArticleController(service, articleService, commentService, objectMapper),
+                        new CommentController(service, articleService, commentService, objectMapper),
+                        new ArticleFreemarkerController(service, articleService, commentService, objectMapper, TemplateFactory.freeMarkerEngine())
+                )
         );
         application.start();
     }
